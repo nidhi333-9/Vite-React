84986236c0b65545e2941bf130c95b7bc5117912
nidhikumari <kumari9936nidhi@gmail.com>
2022-08-13 15:59:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=' text-fuchsia-900 w-full overflow-hidden text-4xl'>
+      <h1>Hello Nidhi</h1>
+    </div>
+  )
+}
+
+export default App
